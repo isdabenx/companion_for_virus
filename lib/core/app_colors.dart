da3a25729d54e242f10f5eab1c9320bb7e06f081
isdabenx <blluch@gmail.com>
2025-04-11 01:42:32 +0200

    feat: :sparkles: Add AppColors class for centralized color management

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Base colors
+  static const Color green = Color(0xFF8BC34A);
+  static const Color blue = Color(0xFF87CEEB);
+  static const Color yellow = Color(0xFFF7DC6F);
+  static const Color red = Color(0xFFFF9380);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color purpleLighter = Color(0xFFe3d9e4);
+  static const Color purpleLight = Color(0xFF9c729d);
+  static const Color purple = Color(0xFF834f85);
+  static const Color purpleDark = Color(0xFF6d3187);
+  static const Color purpleDarker = Color(0xFF682a82);
+  static const Color black = Color(0xFF242424);
+
+  // Menu Home colors
+  static const Color menuOfficialCardsBackgroundColor = green;
+  static const Color menuFanCardsBackgroundColor = blue;
+  static const Color menuRulesBackgroundColor = yellow;
+  static const Color menuFaqBackgroundColor = red;
+  // Menu Home text colors
+  static const Color menuTextColor = white;
+
+  // Theme colors
+  static const Color themeBackgroundColor = purpleLighter;
+
+  // AppBar colors
+  static const Color appBarBackgroundColor = purpleLighter;
+  static const Color appBarTextColor = black;
+}
