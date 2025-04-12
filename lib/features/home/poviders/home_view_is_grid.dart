cd69c016e36c576a58bc9df4c75de6f6acd2b06d
isdabenx <blluch@gmail.com>
2025-04-13 00:23:06 +0200

    feat: :sparkles: Add shared_preferences provider and toggle functionality for grid view state

---
+++ lib/features/home/poviders/home_view_is_grid.dart
@@ -0,0 +1,25 @@
+import 'package:companion_for_virus/core/providers/shared_preferences.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'home_view_is_grid.g.dart';
+
+@riverpod
+class HomeViewIsGrid extends _$HomeViewIsGrid {
+  static const String _key = 'home_view_is_grid';
+
+  @override
+  Future<bool> build() async {
+    final sharedPreferences = await ref.watch(sharedPreferencesProvider.future);
+    return sharedPreferences.getBool(_key) ?? true;
+  }
+
+  Future<void> toogle() async {
+    final SharedPreferences sharedPreferences = await ref.read(
+      sharedPreferencesProvider.future,
+    );
+    final newValue = !(state.value ?? true);
+    await sharedPreferences.setBool(_key, newValue);
+    state = AsyncValue.data(newValue);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:companion_for_virus/core/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
