7620d4c33b944f10e60254249ad1d338d357e05c
isdabenx <blluch@gmail.com>
2025-04-13 00:24:39 +0200

    feat: :sparkles: Refactor HomeScreen to use ConsumerWidget and integrate grid view state management

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,20 +1,18 @@
 import 'package:companion_for_virus/core/app_colors.dart';
 import 'package:companion_for_virus/core/router/app_routes.dart';
+import 'package:companion_for_virus/features/home/poviders/home_view_is_grid.dart';
 import 'package:companion_for_virus/features/home/widgets/menu_card_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    bool isGrid = false;
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue<bool> isGridAsync = ref.watch(homeViewIsGridProvider);
+    final bool isGrid = isGridAsync.value ?? true;
     return Center(
       child: GridView.count(
         shrinkWrap: true,
@@ -29,28 +27,28 @@ class _HomeScreenState extends State<HomeScreen> {
             title: "Official Cards",
             icon: Icons.view_carousel_rounded,
             color: AppColors.menuOfficialCardsBackgroundColor,
-            onTap: () => context.push(AppRoutes.splash),
+            onTap: () => context.push(AppRoutes.officialCards),
           ),
           MenuCardWidget(
             isGrid: isGrid,
             title: "Fan Cards",
             icon: Icons.people_rounded,
             color: AppColors.menuFanCardsBackgroundColor,
-            onTap: () => context.push(AppRoutes.splash),
+            onTap: () => context.push(AppRoutes.fanCards),
           ),
           MenuCardWidget(
             isGrid: isGrid,
             title: "Rules",
             icon: Icons.menu_book_rounded,
             color: AppColors.menuRulesBackgroundColor,
-            onTap: () => context.push(AppRoutes.splash),
+            onTap: () => context.push(AppRoutes.rules),
           ),
           MenuCardWidget(
             isGrid: isGrid,
             title: "FAQ",
             icon: Icons.help_outline_rounded,
             color: AppColors.menuFaqBackgroundColor,
-            onTap: () => context.push(AppRoutes.splash),
+            onTap: () => context.push(AppRoutes.faq),
           ),
         ],
       ),
